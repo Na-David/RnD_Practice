fd5d4ae607eaf9eff864bb3c22223b467bb606f4
David Na <hna206@naver.com>
2022-08-27 23:49:57 +1200

    practice

--- practice.py
+++ practice.py
@@ -5,3 +5,18 @@ print(1000)
 print(3*(3+1))
 
 print(3*(3+1))
+
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(3*(3+1))
+
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(3*(3+1))
+
+print(3*(3+1))
+print(3*(3+1))
