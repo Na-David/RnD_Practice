8b8ba900f847185a2d016359a6008a558b62a725
David Na <hna206@naver.com>
2022-09-01 02:13:22 +1200

    Day2

---
+++ Day2/Day2.py
@@ -0,0 +1,20 @@
+#Data Types
+
+#String
+
+greeting = "Hello"
+print (greeting[3])
+print ("123" + "456")
+
+#Integer
+print(123 + 456)
+
+print (1234_5678_90)
+
+#Float
+print(3.141592)
+
+#Boolean
+
+True
+False
