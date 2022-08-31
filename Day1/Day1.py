221dd683c44bfb13a07b3ef1078acc976b212c21
David Na <hna206@naver.com>
2022-08-31 17:03:44 +1200

    simple debugging

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -5,3 +5,10 @@ print("The function is declared like this:")
 print("print ('what to print')")
 
 print("Hello " + "David!")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
