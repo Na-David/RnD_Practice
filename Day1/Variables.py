53aaab0c4f3ebf9cecda2182f5b57fc98160d15f
David Na <hna206@naver.com>
2022-08-31 17:27:21 +1200

    exercise 1-4

---
+++ Day1/Variables.py
@@ -0,0 +1,15 @@
+name = input("What is your name?")
+print(name)
+
+NAME = "DAVID"
+print(NAME)
+
+name = input("What is your name?")
+print(name)
+
+NAME = "DAVID"
+print(NAME)
+
+print(len(input("What is your name? ")))
+length = len(NAME)
+print (length)
