a11b91cbcf1c559c9470912d526ba07332fe80bd
David Na <hna206@naver.com>
2022-09-01 02:49:25 +1200

    life calculator

---
+++ Day2/Exercise2-3/2-3.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+AGE = 90
+age = int(age)
+
+year = AGE - age
+months = year * 12
+weeks = year * 52
+days = year * 365
+
+print(f"You have {days} days, {weeks} weeks, {months} months left.")
+
+
+
+
+
---
+++ Day2/f-string.py
@@ -0,0 +1,13 @@
+print( round(8/3,4))
+
+# 4 / 2 = float 2.0
+# 4 // 2 = int 2
+
+score = 0
+score -= 5
+score /= 2
+height = 1.7
+isWinning = True
+#f-string
+#print("Your score is " + str(score))
+print(f"your score is {score}, your height is {height}, and you are winning is {isWinning}")
