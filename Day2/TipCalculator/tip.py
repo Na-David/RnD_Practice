62e7a14a354b8c31edfbb916faa95b91ffb056dd
David Na <hna206@naver.com>
2022-09-02 23:52:49 +1200

    Tip Calculator

--- Day2/TipCalculator/tip.py
+++ Day2/TipCalculator/tip.py
@@ -13,9 +13,9 @@ tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill?"))
 
 
-bill_with_tip = tip / 100 * bill + bill # bill * (1 + tip / 100)
+total = tip / 100 * bill + bill # bill * (1 + tip / 100)
 
-bill_per_person = bill_with_tip / people
+bill_per_person = total / people
 final_amount = round(bill_per_person, 2)
 
 print(f"Each person should pay ${final_amount}.")
