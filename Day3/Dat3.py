f6aac311fba3e9a6855f880384d8a9f74912eb17
David Na <hna206@naver.com>
2022-09-21 00:18:34 +1200

    Bill added

--- Day3/Dat3.py
+++ Day3/Dat3.py
@@ -1,11 +1,23 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     age = int(input("What is your age?"))
-    if age >= 18:
-        print("Please pay 8$.")
+    if age <= 12:
+        bill = 5
+        print(f"Please pay ${bill}.")
+    elif age <= 18:
+        bill = 6
+        print(f"Please pay ${bill}.")
     else:
-        print("Please pay $6.")
+        bill = 8
+        print(f"Please pay ${bill}")
+
+    photo = input("Do you want a photo taken? Y or N.")
+    if photo == "Y":
+        bill += 3
+        print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
